0242b1bf1eb8aa300db4deff7f6c35940a094e6a
limk <lmk57320402@126.com>
2018-09-09 08:09:16 +0800

    update eureka

--- microservice-eureka/src/main/java/com/limk/cloud/microserviceeureka/MicroserviceEurekaApplication.java
+++ microservice-eureka/src/main/java/com/limk/cloud/microserviceeureka/MicroserviceEurekaApplication.java
@@ -2,8 +2,10 @@ package com.limk.cloud.microserviceeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MicroserviceEurekaApplication {
 
     public static void main(String[] args) {
