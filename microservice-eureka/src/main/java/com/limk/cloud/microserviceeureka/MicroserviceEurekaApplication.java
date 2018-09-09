7e3e573f0d7622919f9fe2f2fc2c71ddbdba4c1e
limk <lmk57320402@126.com>
2018-09-09 08:02:52 +0800

    add eureka

---
+++ microservice-eureka/src/main/java/com/limk/cloud/microserviceeureka/MicroserviceEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.limk.cloud.microserviceeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceEurekaApplication.class, args);
+    }
+}
