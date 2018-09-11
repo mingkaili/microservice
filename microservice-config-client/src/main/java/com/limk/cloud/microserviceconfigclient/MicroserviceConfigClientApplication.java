6681621899a8c2f4fc3e100aee28018aef7fcd3c
lmk57320402@126.com <lmk57320402@126.com>
2018-09-11 17:53:01 +0800

    update config file

---
+++ microservice-config-client/src/main/java/com/limk/cloud/microserviceconfigclient/MicroserviceConfigClientApplication.java
@@ -0,0 +1,12 @@
+package com.limk.cloud.microserviceconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceConfigClientApplication.class, args);
+    }
+}
---
+++ microservice-config-client/src/main/java/com/limk/cloud/microserviceconfigclient/controller/ConfigClientController.java
@@ -0,0 +1,18 @@
+package com.limk.cloud.microserviceconfigclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigClientController {
+
+    @Value("${com.limk}")
+    private String hello;
+
+    @RequestMapping("/hello")
+    public String from() {
+        return this.hello;
+    }
+}
+
